f7c9e6e294ffbdb5ff336d32d7f1e126f841fa31
kim <yuyukgh@naver.com>
2022-08-12 22:14:59 +0900

    [Feat] 모바일 메뉴 업데이트

--- src/components/global/header/index.js
+++ src/components/global/header/index.js
@@ -2,9 +2,11 @@ import { useEffect, useState, useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
+import { BsList } from 'react-icons/bs';
 
 import logoWhiteSrc from '../../../asset/images/global/logo_white.png';
 import logoBlackSrc from '../../../asset/images/global/logo_black.png';
+import MobileGnbComponent from '../mobileGnb';
 
 import { getPageH1Title } from './util';
 import styles from './index.module.scss';
@@ -14,6 +16,7 @@ const cx = classNames.bind(styles);
 const Header = () => {
   const [scrollY, setScrollY] = useState(window.scrollY);
   const [isScrollUp, setIsScrollUp] = useState(false);
+  const [isMobileGnb, setIsMobileGnb] = useState(false);
   const { pathname } = useLocation();
 
   const ITEM_PAGE_ARR = ['/product/detail/BLEU', '/product/detail/VERT', '/product/detail/ROUGE', '/product/detail/ROSE'];
@@ -66,27 +69,38 @@ const Header = () => {
     return 'white';
   };
 
+  const handleShowMobileGnb = () => {
+    setIsMobileGnb(true);
+  };
   return (
-    <header className={cx(['header', isScrollUp && 'active'])}>
-      <ul className={cx('gnb')}>
-        <li className={cx(setIsBlackColor())}>
-          <h1 onClick={scrollTop}>
-            <Link to="/">
-              <img src={logoSrcCreator()} alt={getPageH1Title(pathname)} />
-            </Link>
-          </h1>
-        </li>
-        <li className={cx(setIsBlackColor())}>
-          <Link to="/about">ABOUT</Link>
-        </li>
-        <li className={cx(setIsBlackColor())}>
-          <Link to="/product">PRODUCT</Link>
-        </li>
-        <li className={cx(setIsBlackColor())}>
-          <Link to="/about#contact">CONTACT</Link>
-        </li>
-      </ul>
-    </header>
+    <>
+      <header className={cx(['header', isScrollUp && 'active'])}>
+        <ul className={cx('gnb')}>
+          <li className={cx([setIsBlackColor(), 'logo'])}>
+            <h1 onClick={scrollTop}>
+              <Link to="/">
+                <img src={logoSrcCreator()} alt={getPageH1Title(pathname)} />
+              </Link>
+            </h1>
+          </li>
+          <li className={cx(setIsBlackColor())}>
+            <Link to="/about">ABOUT</Link>
+          </li>
+          <li className={cx(setIsBlackColor())}>
+            <Link to="/product">PRODUCT</Link>
+          </li>
+          <li className={cx(setIsBlackColor())}>
+            <Link to="/about#contact">CONTACT</Link>
+          </li>
+          <li className={cx([setIsBlackColor(), 'mobileButton'])}>
+            <button type="button" onClick={handleShowMobileGnb}>
+              <BsList />
+            </button>
+          </li>
+        </ul>
+      </header>
+      {isMobileGnb && <MobileGnbComponent setIsMobileGnb={setIsMobileGnb} />}
+    </>
   );
 };
 
